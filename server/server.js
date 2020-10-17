08f55353671b3f28582714d0b445b427a8ed741e
barhouum7 <bb.med2000@gmail.com>
2020-10-18 00:11:38 +0100

    Added Some Changes on the server

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const app = express() /* Get the App Portion of Express */
 const socketIo = require('socket.io')
 const http = require('http')
 
+const PORT = process.env.PORT || 5000
 
 
 const server = http.createServer(app)
 const io = socketIo(server)
 
+app.listen(PORT, (error) => {
+    error ?
+        console.log(`\nERROR! Something Went Wrong.\n`) :
+        console.log(`\nServer is listening on PORT: ${PORT}\n`)
+}) /* Telling to our App that we want to Listening on Certain PORT */
