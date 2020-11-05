9fb63ca0d4f24b128841ec23965aca6432345d5e
barhouum7 <bb.med2000@gmail.com>
2020-11-05 23:59:56 +0100

    modified:   client/src/components/Chat/Chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
 const Chat = () => {
+    useEffect(({location}) => {
+        const data = queryString.parse(location.search)
+    })
+
     return (
         <h1>This is the Chat Component.</h1>
     )
