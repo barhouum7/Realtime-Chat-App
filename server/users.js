6d866e5643c70c7a92b5287f6d6e06932399de50
barhouum7 <bb.med2000@gmail.com>
2020-11-14 20:59:36 +0100

    Fully completed setting up  server/users.js file

--- server/users.js
+++ server/users.js
@@ -17,15 +17,12 @@ const addUser = ({id, name, room}) => {
 }
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id === id);
-  
-    if(index !== -1) return users.splice(index, 1)[0];
-  }
-
-const getUser = () => {
-
+    const index = users.findIndex((user) => user.id === id)
+    if (index !== -1) return users.splice(index, 1)[0]
 }
 
-const getUsersInRoom = () => {
+const getUser = (id) => users.find((user) => user.id === id)
 
-}
+const getUsersInRoom = (room) => users.filter((user) => user.room === room)
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
