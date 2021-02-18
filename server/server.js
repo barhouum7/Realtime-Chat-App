6961fb27b856a41f87e568a92d840b8f13f67408
IbrahimBHMBS <bb.med2000@gmail.com>
2021-02-18 03:08:58 +0100

    SERVER: modified:  server/server.js

--- server/server.js
+++ server/server.js
@@ -22,6 +22,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) } )
+
         callback()
     })
 
@@ -29,12 +31,18 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id)
 
         io.to(user.room).emit('message', {user: user.name, text: message})
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) } )
 
         callback()
     })
 
     socket.on('disconnect', () => {
-    console.log('\n\n\tUser had left!!!')
+    // console.log('\n\n\tUser had left!!!')
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has left!` } )
+        }
     })
 })
 
