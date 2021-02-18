b51e03d230a966f977921f118149135caf67475f
IbrahimBHMBS <bb.med2000@gmail.com>
2021-02-18 03:16:46 +0100

    CLIENT: Added an InfoBar Component

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 import './InfoBar.css'
+import '../../css/uiFont.css'
 
-const InfoBar = () => {
+const InfoBar = ({room, name}) => {
     
+    return (
+            <div className="infoBar">
+                <a href="/"><i className="closeIcon icon-exit_to_app"></i></a>
+                <div className="leftInnerContainer">
+                    <h3><i className="personIcon icon-person"></i>{name} - State (Online)<br /><i className="personIcon icon-meeting_room"></i> {room} Talk</h3>
+                </div>
+                
+                <div className="iconsContainer">
+                    <a href="#!"><i className="callIcon icon-call"></i></a>
+                    <a href="#!"><i className="callIcon icon-video-camera"></i></a>
+                </div>
+            </div>
+    )
+        
 }
 
 export default InfoBar
